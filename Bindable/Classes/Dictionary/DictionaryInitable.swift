b59ce8295230bd6b5426965d58f3a68de5bdc6cc
Leif Meyer <leif@incipia.co>
2017-07-07 10:23:41 -0700

    Added dictionary initable and representable protocols.

---
+++ Bindable/Classes/Dictionary/DictionaryInitable.swift
@@ -0,0 +1,89 @@
+//
+//  DictionaryInitable.swift
+//  Pods
+//
+//  Created by Leif Meyer on 7/7/17.
+//
+//
+
+import Foundation
+
+public enum IncDictionaryError: Error {
+   case invalidType(selfType: String, valueType: String)
+   case invalidValue(selfType: String, value: String)
+}
+
+public protocol IncDictionaryInitable {
+   init?(dictionary: Any) throws
+}
+
+public protocol IncKVDictionaryInitable: IncDictionaryInitable, IncKVCompliance {
+   static var dictionaryKeys: [Key] { get }
+   static var dictionaryPath: [String] { get }
+   init()
+}
+
+public enum IncKVDictionaryError: Error {
+   case underlyingError(selfType: String, key: String, error: Error)
+}
+
+public extension IncDictionaryInitable {
+   // MARK: - Error creation
+   static func dictionaryTypeError(value: Any) -> IncDictionaryError { return .invalidType(selfType: "\(Self.self)", valueType: "\(type(of: value))") }
+   
+   static func dictionaryValueError(value: Any) -> IncDictionaryError { return .invalidValue(selfType: "\(Self.self)", value: "\(value)") }
+}
+
+public extension IncKVDictionaryInitable {
+   static var dictionaryKeys: [Key] { return Key.all }
+   static var dictionaryPath: [String] { return [] }
+   
+   static func kvDictionaryUnderlyingError(key: Key, error: Error) -> IncKVDictionaryError { return .underlyingError(selfType: "\(Self.self)", key: key.rawValue, error: error) }
+
+   init(dictionary: [String : Any]) throws {
+      var dictionary = dictionary
+      try Self.dictionaryPath.forEach {
+         guard let value = dictionary[$0] as? [String : Any] else { throw Self.dictionaryTypeError(value: dictionary) }
+         dictionary = value
+      }
+      self.init()
+      try update(with: dictionary)
+   }
+   
+   mutating func update(with dictionary: [String : Any]) throws {
+      try Self.dictionaryKeys.forEach {
+         do {
+            try self.set(value: dictionary[$0.rawValue], for: $0)
+         } catch {
+            throw Self.kvDictionaryUnderlyingError(key: $0, error: error)
+         }
+      }
+   }
+}
+
+public protocol IncKVDictionaryInitableClass: class, IncKVDictionaryInitable, IncKVComplianceClass {}
+
+public extension IncKVDictionaryInitableClass {
+   func update(with dictionary: [String : Any]) throws {
+      try Self.dictionaryKeys.forEach {
+         do {
+            try self.set(value: dictionary[$0.rawValue], for: $0)
+         } catch {
+            throw Self.kvDictionaryUnderlyingError(key: $0, error: error)
+         }
+      }
+   }
+}
+
+public protocol IncKVDictionaryInitableRepresentable: IncKVDictionaryInitable, IncKVDictionaryRepresentable {}
+
+public extension IncKVDictionaryInitableRepresentable {
+   static var dictionaryKeys: [Key] { return Key.all }
+}
+
+public protocol IncKVDictionaryInitableRepresentableClass: IncKVDictionaryInitableClass, IncKVDictionaryRepresentable {}
+
+public extension IncKVDictionaryInitableRepresentableClass {
+   static var dictionaryKeys: [Key] { return Key.all }
+}
+
---
+++ Bindable/Classes/Dictionary/DictionaryRepresentable.swift
@@ -0,0 +1,42 @@
+//
+//  DictionaryRepresentable.swift
+//  Pods
+//
+//  Created by Leif Meyer on 7/7/17.
+//
+//
+
+import Foundation
+
+public protocol IncDictionaryRepresentable {
+   var dictionaryRepresentation: [String : Any]? { get }
+}
+
+public protocol IncKVDictionaryRepresentable: IncJSONRepresentable, IncKVCompliance {
+   static var dictionaryKeys: [Key] { get }
+}
+
+public extension IncKVJSONRepresentable {
+   static var dictionaryKeys: [Key] { return Key.all }
+
+   var dictionaryRepresentation: [String : Any]? {
+      var dictionary: [String : Any] = [:]
+      Self.dictionaryKeys.forEach {
+         dictionary[$0.rawValue] = self.value(for: $0)
+      }
+      return dictionary
+   }
+
+   var deepDictionaryRepresentation: [String : Any]? {
+      var dictionary: [String : Any] = [:]
+      Self.dictionaryKeys.forEach {
+         let value = self.value(for: $0)
+         if let representableValue = value as? IncDictionaryRepresentable {
+            dictionary[$0.rawValue] = representableValue.dictionaryRepresentation
+         } else {
+            dictionary[$0.rawValue] = value
+         }
+      }
+      return dictionary
+   }
+}
